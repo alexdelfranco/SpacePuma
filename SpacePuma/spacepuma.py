0c8e517cee88f1dbd84b4fdc820864b2a1c675e9
Alex DelFranco <alexdelfranco@Alexs-MacBook-Air.local>
2022-09-28 10:51:50 -0400

    Added __init__.py file

--- SpacePuma/spacepuma.py
+++ SpacePuma/spacepuma.py
@@ -3,7 +3,6 @@
 # Institution: Center for Astrophysics | Harvard & Smithsonian
 # Date: 27 September 2022
 
-import sys; sys.path.append('src')
 from main_menu import main_menu
 
 class puma(main_menu):
