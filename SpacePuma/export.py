5522400abf0f8b029300e4e200668aae7ff60429
Alex DelFranco <alexdelfranco@Alexs-Air.lan>
2022-12-06 15:13:39 -0500

    Fixed export data menus

--- SpacePuma/export.py
+++ SpacePuma/export.py
@@ -118,44 +118,9 @@ class export(widget_base):
 
         #####################
 
-        param_selects = []
-        for num in range(self.dict_depth(self.main_menu.get_data())):
-            button = widgets.Select(options=[],rows=0,style={'description_width': 'initial'},layout = widgets.Layout(width='150px'))
-            param_selects.append(button)
+        self.param_selects = []
 
-        # Define a function used to setup the export buttons
-        def data_select(self,param_selects,index):
-            # Define a function for the first few buttons
-            def func(b,self=self):
-                # Make sure the function does not repeatedly get called
-                if param_selects[index].value is None: return
-                elif param_selects[index].value == 'All':
-                    self.place_menu(self.param_menu,param_selects[:index+1])
-                    return
-                # Set the param_select values of the deeper selects to None
-                for select in param_selects[index+1:]: select.value = None
 
-                # Clear the data path following the current entry
-                self.info['datadict_path'] = self.info['datadict_path'][:index]
-                # Add the current entry to the data path
-                self.info['datadict_path'].append(param_selects[index].value)
-
-                # Define a subset of the data
-                sub_data = self.main_menu.get_data(*self.info['datadict_path'])
-                if type(sub_data) == dict and index < len(param_selects)-1:
-                    # For the next selection, set the option to the keys of the selected dictionary
-                    param_selects[index+1].options = ['All'] + list(sub_data.keys())
-                    # Place an updated menu
-                    self.place_menu(self.param_menu,param_selects[:index+2]+[self.save_data_button])
-                else:
-                    # Place an updated menu
-                    self.place_menu(self.param_menu,param_selects[:index+1]+[self.save_data_button])
-                    pass
-
-            return func
-
-        for index,select in enumerate(param_selects):
-            select.observe(data_select(self,param_selects,index))
 
         #####################
 
@@ -189,6 +154,57 @@ class export(widget_base):
 
         #####################
 
-        return [self.export_path_text,self.export_data_button,self.export_all_button,self.save_button,self.dpi_text],[self.export_data_button],param_selects
+        return [self.export_path_text,self.export_data_button,self.export_all_button,self.save_button,self.dpi_text],[self.export_data_button],self.param_selects
 
     def update(self,artists,data): return
+
+    # Define a function used to setup the export buttons
+    def data_select(self,param_selects,index):
+        # Define a function for the first few buttons
+        def func(b,self=self):
+            # Make sure the function does not repeatedly get called
+            if param_selects[index].value is None: return
+            elif param_selects[index].value == 'All':
+                self.place_menu(self.param_menu,param_selects[:index+1]+[self.save_data_button])
+                return
+            # Set the param_select values of the deeper selects to None
+            for select in param_selects[index+1:]: select.value = None
+
+            # Clear the data path following the current entry
+            self.info['datadict_path'] = self.info['datadict_path'][:index]
+            # Add the current entry to the data path
+            self.info['datadict_path'].append(param_selects[index].value)
+
+            # Define a subset of the data
+            sub_data = self.main_menu.get_data(*self.info['datadict_path'])
+            self.sub_data = sub_data
+            self.index = index
+            self.param_selects = param_selects
+            if type(sub_data) == dict and index < len(param_selects)-1:
+                # For the next selection, set the option to the keys of the selected dictionary
+                param_selects[index+1].options = ['All'] + list(sub_data.keys())
+                # Place an updated menu
+                self.place_menu(self.param_menu,param_selects[:index+2]+[self.save_data_button])
+            else:
+                # Place an updated menu
+                self.place_menu(self.param_menu,param_selects[:index+1]+[self.save_data_button])
+                pass
+
+        return func
+
+    # Override activation method
+    def activate(self):
+        # Activate the widget
+        self.widget_on = True
+        self.place_menu(self.menu,self.button_list)
+        # Setup parameter selects for saving data
+        self.param_selects = []
+        for num in range(self.dict_depth(self.main_menu.get_data())):
+            button = widgets.Select(options=[],rows=0,style={'description_width': 'initial'},layout = widgets.Layout(width='150px'))
+            self.param_selects.append(button)
+        # Update the param_buttons as well
+        self.param_buttons = self.param_selects
+        for index,select in enumerate(self.param_selects):
+            select.observe(self.data_select(self.param_selects,index))
+        # Return the widget toggle
+        return self.widget_on
